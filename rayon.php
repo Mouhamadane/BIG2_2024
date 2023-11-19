ba1c262e155b877c7648fc23340103bec87f1f6a
Mouhamadane MBOUP <danelegrand@gmail.com>
2023-11-19 12:08:35 +0000

    calcul du diam

---
+++ rayon.php
@@ -0,0 +1,20 @@
+<?php 
+    $rayon = (int) $_POST["number"];
+    $diametre = $rayon*2;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 1</title>
+</head>
+<body>
+   <form method="POST">
+    <label for="">Entrer le rayon :</label>
+    <input type="text" name="number">
+    <button type="submit">Envoyer</button>
+   </form>
+    <p>Le diamètre est : <?php echo $diametre ?></p>
+</body>
+</html>
